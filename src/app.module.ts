73e405f63f93ad70a48a49c9c2c305352e69e228
Francisco Santamaria <sm19035@ues.edu.sv>
2025-01-13 13:26:05 -0600

    agregando cambios a users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, UsersModule],
   controllers: [],
   providers: [],
   exports: [],
