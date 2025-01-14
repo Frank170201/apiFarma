fecd0a02801afdfd1ae3dd92d931483b124964a4
Francisco Santamaria <sm19035@ues.edu.sv>
2025-01-14 13:50:49 -0600

    agregando cambios a modulo de usuarios

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { IsBoolean, IsEmail, IsString, MinLength } from "class-validator";
+
+export class CreateUserDto {
+    @IsString()
+    @MinLength(1)
+    name: string;
+    @IsString()
+    @IsEmail()
+    email: string;
+    @IsString()
+    @MinLength(6)
+    password: string;
+    @IsBoolean()
+    status: boolean;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class UsersService {
+
+  private users: User[]=[
+    {
+      id: uuid(),
+      name: 'John Doe',
+      email: 'fran170201@gmail.com',
+      password: '123456',
+      status: true,
+      createdAt: new Date()
+    }
+  ]
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const {name, email, password, status} = createUserDto;
+    const user:User = {
+      id: uuid(),
+      name: name.toLocaleLowerCase(),
+      email: email,
+      password: password,
+      status: status,
+      createdAt: new Date()
+    }
+    this.users.push(user);
+    return user;
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.users;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    const user= this.users.find((user)=> user.id === id);
+    if(!user){
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: string, updateUserDto: UpdateUserDto) {
+    let userDB = this.findOne(id);
+    this.users = this.users.map(user=>{
+      if(user.id===id){
+        userDB.updatedAt = new Date();
+        userDB = {...userDB, ...updateUserDto};
+        return userDB;
+      }
+      return user;
+    })
+    return userDB;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: string) {
+    this.users = this.users.filter((user)=> user.id !== id);
   }
 }
