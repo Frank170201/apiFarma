3fcc187787abc479d52799bc51571108a7b14c28
Francisco Santamaria <sm19035@ues.edu.sv>
2025-01-15 14:18:46 -0600

    agregandos cambios

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { SeedModule } from './seed/seed.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [ UsersModule, SeedModule],
+  imports: [ UsersModule,
+    // MongooseModule.forRoot('mongodb://localhost:27017/apiFarma', ),
+    SeedModule,
+    CommonModule],
   controllers: [],
   providers: [],
   exports: [],
