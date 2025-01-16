8033faa9f3e533576305b2c61c9e2a7eedb28b42
Francisco Santamaria <sm19035@ues.edu.sv>
2025-01-16 09:00:17 -0600

    cambios

---
+++ src/common/adapters/axios.adapter.ts
@@ -0,0 +1,13 @@
+import { HttpAdapter } from '../interfaces/http-adapter.interface';
+import axios, { AxiosInstance } from 'axios';
+
+export class AxiosAdapter implements HttpAdapter {
+  private axios: AxiosInstance = axios;
+  async get<T>(url: string): Promise<T> {
+    try {
+        const { data } = await this.axios.get<T>(url);
+    } catch (error) {
+        
+    }
+  }
+}
